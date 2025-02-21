2a809e8403428a84d80f23fc420b477ada557431
manishyadav2296 <anshkyadav2296@gmail.com>
2025-02-21 19:04:37 +0530

    js

---
+++ DOMS/test.js
@@ -0,0 +1,9 @@
+ let btn1=document.querySelector("#btn1");
+ btn1.onclick=(evt)=>{
+    console.log(evt);
+    console.log(evt.type)
+    console.log(evt.target);
+    console.log(evt.clientX, evt.clientY);
+    
+ }
+
---
+++ istpractise/test.js
@@ -0,0 +1,196 @@
+//const student={
+  //  name:"ANsh",
+    //cgpa:8.6,
+    //age:25,
+    //ispass:true
+//};
+//console.log(student)
+
+//let a=5;//number
+//let b="5";//string
+//console.log("a===b",a===b);
+
+
+//let a=10;//num
+//let b=20;//num
+//let c=30.5;//float
+//let d="Manish";//string
+//let e="gandhi";//string
+//let f=true;//boolean
+//let g=false;//boolean
+//let h;//undefine
+//console.log(a+b);
+//console.log(a+c);
+//console.log(a+d);
+//console.log(a+e);
+//console.log(a+f);
+//console.log(a+g);
+//console.log(a+h);
+
+//let a=5;
+//let b=6;
+//let cond1=b>a;
+//let cond2=a===5
+//console.log("cond1 || cond2=" , cond1 || cond2);
+
+
+//let a=20;
+//if (a%2===0) {
+//console.log("even")    
+//}else{
+  //  console.log("not even")
+//}
+
+
+//let age=10;
+//if (age>18) {
+//console.log("youth");    
+//}else if(age>45){
+  //  console.log("middle");
+//} else{
+  //  console.log("child");
+//}
+
+
+
+//let a=14;
+//let result=a%2===0 ? "even":"odd"
+//console.log(result);
+
+///let a=18;
+//a%2===0 ? console.log("even"):console.log("odd");
+
+//let sum=0;
+//let n=8
+//for(let i=1;i<=n;i++){
+  //  sum=sum+i;
+
+//}
+//console.log("sum=",sum)
+
+//let i=10;
+//while (i>=0) {
+  //  console.log(i)
+    //i--;
+    
+//}
+
+
+//let i=8;
+//do{
+  //  console.log("i=",i)
+    //i++;
+//}while(i<=9);
+
+//let str="MANISH";
+
+//for(let i of str){
+
+//console.log("i=",i )
+//}console.log(str.length);
+
+//let student={
+  //  name:"MANISH",
+    //age:19,
+    //cgpa:8,
+    //ispass:true
+//};
+//for(let key in student){
+  //  console.log("key=",key, "value=",(student[key]))
+//}
+
+//for(let i=0; i<+100;i++){
+  //  if(i%2!==0)
+    //    console.log(i)
+//}
+
+//let gameNum=7;
+//let userNum=prompt("please enter the number:");
+//while(userNum!==gameNum){
+  //  userNum=prompt("please enter correct number")
+//}
+//console.log("congratulation ");
+
+//let str="MANISH";
+//console.log(str[3]);
+//let specialString=`MANISH`
+//console.log( typeof specialString)
+
+//let obj={
+  //  item:"pen",
+    //price:10,
+//};
+//let output=`the cost of ${obj.item} is ${obj.price}  rs`
+//console.log(output);
+
+//console.log("MANISH\nYADAV");
+//console.log("MANISH\tYADAV")
+
+//let str="anshkumaryadav";
+//str=str.toUpperCase();
+//console.log(str);
+
+//let str="MANISHKUMARYADAV";
+//str=str.toLowerCase();
+//console.log(str);
+
+
+//let str="   Manish    kumar"
+//console.log(str.trim());
+
+//let str="Manish"
+//console.log(str.slice(1,4));
+
+//let str1="Manish"
+//let str2="Yadav"
+//str=str2.concat(str1);
+//console.log(str)
+
+//let str="Yellow"
+//str=str.replace("Y", "H")
+//console.log(str);
+
+//let str="HELLOW"
+//console.log(str.charAt(3));
+
+//let arr=[20,98,76,45,45,70,24,10,72];
+//console.log(arr);
+//console.log(arr.length);
+
+//let heroes=["super man","iron man","hulk","antman","black widow"]
+//for(let i=0; i<heroes.length;i++)
+    //console.log(heroes[i]);
+
+//let marks=[85,97,44,37,76,60];
+//sum=0;
+//for(let val of marks ){
+  //  sum=sum+val;
+//}
+//let avg= sum/marks.length;
+//console.log(`avg marks of class= ${avg}`);
+
+
+//let items=[250,645,300,900,50];
+//for(i=0; i<items.length; i++){
+  //  let offer=items[i]/10;
+    //items[i]-=offer;
+//}
+//console.log(items);
+
+//let items=["tomato","potato","pnion,","chips","mango","key","pen"]
+
+//console.log(items.pop());
+//console.log(items);
+
+//let num=[101,109,167,876,9532];
+//console.log(num.toString());
+
+//let Marvelheroes=["ironman","superman","spiderman","antman"];
+
+  //console.log(Marvelheroes.shift())  
+  //console.log(Marvelheroes)
+
+  let int=[9,876,654,234,98];
+  int.splice(2,2,101,102);
+  console.log(int)
+    
